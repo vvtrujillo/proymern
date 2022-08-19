b024abb244230bf1deb4511b05864a88f2a52224
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-08-18 21:35:35 -0400

    version 1

---
+++ client/src/Components/jugador/JugadorAdmin.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+import { Route, Router } from "react-router-dom";
+
+
+const JugadorAdmin = () => {
+    
+    const[datos, setDatos] = useState([]);
+
+    return(
+        <React.Fragment>
+            <Router>
+                <Route path="Listado" element={<Listado datos={datos}></Listado>}></Route>
+                <Col></Col>
+            </Router>
+        </React.Fragment>
+    )
+}
+
+export default JugadorAdmin;
